7f9096735ec697a09dd605536ddd0d7fcf157511
Raviteja Emandi <raviteja_emandi@prod.hclpnp.com>
2024-07-02 12:02:43 +0530

    Second Mothod added 1.0.2

--- app/src/main/java/com/test/samplelib/MainActivity.java
+++ app/src/main/java/com/test/samplelib/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         HelloWordl.printLog("From_SDK", "Hello from SDK");
+        HelloWordl.printRaviTeja("test", "testRavi");
     }
 }
